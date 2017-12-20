806d04bc6bd75c148f0a46448a525d211f059743
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-12-20 02:25:49 +0000

    #TTTS-172 ### Added - パフォーマンスの日時とツアーの情報を強化。

--- app/controllers/performance.js
+++ app/controllers/performance.js
@@ -16,12 +16,13 @@ const ttts = require("@motionpicture/ttts-domain");
 const createDebug = require("debug");
 const fs = require("fs-extra");
 const moment = require("moment");
+// tslint:disable-next-line:no-require-imports no-var-requires
+require('moment-timezone');
 const debug = createDebug('ttts-jobs:controller:performance');
 /**
  * 設定からパフォーマンスデータを作成する
  * @memberof controller/performance
  */
-// tslint:disable-next-line:max-func-body-length
 function createFromSetting() {
     return __awaiter(this, void 0, void 0, function* () {
         // 作成情報取得
@@ -29,13 +30,9 @@ function createFromSetting() {
         debug('setting:', setting);
         // 引数情報取得
         const targetInfo = getTargetInfoForCreateFromSetting(setting.performance_duration, setting.no_performance_times);
-        const times = targetInfo.times;
-        const days = targetInfo.days;
         debug('targetInfo:', targetInfo);
         // 劇場とスクリーン情報取得
-        const screenOfPerformance = yield ttts.Models.Screen.findById(setting.screen)
-            .populate('theater')
-            .exec();
+        const screenOfPerformance = yield ttts.Models.Screen.findById(setting.screen).populate('theater').exec();
         debug('screenOfPerformance:', screenOfPerformance);
         if (screenOfPerformance === null) {
             throw new Error('screen not found.');
@@ -50,65 +47,63 @@ function createFromSetting() {
         const performanceRepo = new ttts.repository.Performance(ttts.mongoose.connection);
         const savePerformances = [];
         // 7日分Loop
-        const promisesDay = (days.map((day) => __awaiter(this, void 0, void 0, function* () {
-            // 開始時間分Loop
-            const promisesTime = (times.map((time) => __awaiter(this, void 0, void 0, function* () {
-                // 2017/10 2次 予約枠、時間の変更対応
-                const screen = (setting.special_screens[time.start_time] !== undefined) ?
-                    setting.special_screens[time.start_time] : setting.screen;
-                // ユニークなIDを生成
-                const id = [
-                    // tslint:disable-next-line:no-magic-numbers
-                    day.slice(-6),
-                    setting.film,
-                    screen,
-                    time.start_time
-                ].join('');
-                // パフォーマンス登録
-                const performance = {
-                    id: id,
-                    theater: screenOfPerformance.get('theater').get('id'),
-                    theater_name: screenOfPerformance.get('theater').get('name'),
-                    screen: screen,
-                    screen_name: screenOfPerformance.get('name'),
-                    film: film.get('id'),
-                    ticket_type_group: setting.ticket_type_group,
-                    day: day,
-                    open_time: time.open_time,
-                    start_time: time.start_time,
-                    end_time: time.end_time,
-                    canceled: false,
-                    ttts_extension: {
-                        tour_number: time.tour_number,
-                        ev_service_status: ttts.factory.performance.EvServiceStatus.Normal,
-                        ev_service_update_user: '',
-                        online_sales_status: ttts.factory.performance.OnlineSalesStatus.Normal,
-                        online_sales_update_user: '',
-                        refund_status: ttts.factory.performance.RefundStatus.None,
-                        refund_update_user: '',
-                        refunded_count: 0
-                    }
-                };
-                debug('creating performance...', performance);
-                //スクリーン、作品、上映日、開始時間
-                yield performanceRepo.saveIfNotExists(performance);
-                savePerformances.push(performance);
-            })));
-            yield Promise.all(promisesTime);
+        yield Promise.all(targetInfo.map((performanceInfo) => __awaiter(this, void 0, void 0, function* () {
+            const day = moment(performanceInfo.start_date).tz('Asia/Tokyo').format('YYYYMMDD');
+            const startTime = moment(performanceInfo.start_date).tz('Asia/Tokyo').format('HHmm');
+            const endTime = moment(performanceInfo.end_date).tz('Asia/Tokyo').format('HHmm');
+            // 2017/10 2次 予約枠、時間の変更対応
+            const screen = (setting.special_screens[startTime] !== undefined) ? setting.special_screens[startTime] : setting.screen;
+            // ユニークなIDを生成
+            const id = [
+                // tslint:disable-next-line:no-magic-numbers
+                day.slice(-6),
+                setting.film,
+                screen,
+                startTime
+            ].join('');
+            // パフォーマンス登録
+            const performance = {
+                id: id,
+                theater: screenOfPerformance.get('theater').get('id'),
+                theater_name: screenOfPerformance.get('theater').get('name'),
+                screen: screen,
+                screen_name: screenOfPerformance.get('name'),
+                film: film.get('id'),
+                ticket_type_group: setting.ticket_type_group,
+                day: day,
+                open_time: startTime,
+                start_time: startTime,
+                end_time: endTime,
+                canceled: false,
+                ttts_extension: {
+                    tour_number: performanceInfo.tour.code,
+                    ev_service_status: ttts.factory.performance.EvServiceStatus.Normal,
+                    ev_service_update_user: '',
+                    online_sales_status: ttts.factory.performance.OnlineSalesStatus.Normal,
+                    online_sales_update_user: '',
+                    refund_status: ttts.factory.performance.RefundStatus.None,
+                    refund_update_user: '',
+                    refunded_count: 0
+                },
+                door_time: performanceInfo.door_time,
+                start_date: performanceInfo.start_date,
+                end_date: performanceInfo.end_date,
+                duration: performanceInfo.duration,
+                tour: performanceInfo.tour
+            };
+            debug('creating performance...', performance);
+            yield performanceRepo.saveIfNotExists(performance);
+            savePerformances.push(performance);
         })));
-        yield Promise.all(promisesDay);
     });
 }
 exports.createFromSetting = createFromSetting;
 /**
  * パフォーマンス作成・作成対象情報取得
- *
  * @memberof controller/performance
  */
 function getTargetInfoForCreateFromSetting(duration, noPerformanceTimes) {
-    const info = {};
-    info.days = [];
-    info.times = [];
+    const performanceInfos = [];
     // 引数から作成対象時間と作成日数を取得
     const argvLength = 5;
     if (process.argv.length < argvLength) {
@@ -123,40 +118,46 @@ function getTargetInfoForCreateFromSetting(duration, noPerformanceTimes) {
     const start = Number(process.argv[indexStartDay]);
     // 何日分作成するか: 7
     const days = Number(process.argv[indexTargetDays]);
-    // 本日日付+開始日までの日数から作成開始日セット
-    const today = moment().add(start - 1, 'days');
-    // 作成日数分の作成対象日付作成
-    for (let index = 0; index < days; index = index + 1) {
-        const dateWk = today.add(1, 'days').format('YYYYMMDD');
-        info.days.push(dateWk);
-    }
     const minutes = ['00', '15', '30', '45'];
     const tours = ['1', '2', '3', '4'];
-    const hourLength = 2;
-    // 終了時刻取得
-    const getEndTime = (startTime) => {
-        const startMoment = moment(startTime, 'HHmm');
-        return moment(startMoment.add(duration, 'minutes'), 'HHmm').format('HHmm');
-    };
-    hours.forEach((hour) => {
-        // 2桁でない時は'0'詰め
-        hour = (hour.length < hourLength) ? `0${hour}` : hour;
-        let index = 0;
-        minutes.forEach((minute) => {
-            const startTime = hour + minute;
-            // パフォーマンスを作成しない時刻に指定されていなかったら作成
-            if (noPerformanceTimes.indexOf(startTime) < 0) {
-                info.times.push({
-                    open_time: startTime,
-                    start_time: startTime,
-                    end_time: getEndTime(startTime),
-                    tour_number: `${hour}${tours[index]}`
-                });
-            }
-            index += 1;
+    // 本日日付+開始日までの日数から作成開始日セット
+    // 作成日数分の作成対象日付作成
+    for (let index = 0; index < days; index = index + 1) {
+        const now = moment().add(start + index, 'days');
+        hours.forEach((hour) => {
+            // 2桁でない時は'0'詰め
+            // tslint:disable-next-line:no-magic-numbers
+            hour = `0${hour}`.slice(-2);
+            const tourStartDate = moment(`${now.format('YYYYMMDD')} ${hour}:00:00+09:00`, 'YYYYMMDD HH:mm:ssZ');
+            minutes.forEach((minute, minuteIndex) => {
+                // ツアー情報作成
+                const tourCode = `${hour}${tours[minuteIndex]}`;
+                const tour = {
+                    name: {
+                        en: `Tour${tourCode}`,
+                        ja: `ツアー${tourCode}`
+                    },
+                    code: `${tourCode}`,
+                    door_time: tourStartDate.toDate(),
+                    start_date: tourStartDate.toDate(),
+                    end_date: moment(tourStartDate).add(1, 'hour').toDate(),
+                    duration: moment.duration(1, 'hour').toISOString()
+                };
+                const startDate = moment(`${now.format('YYYYMMDD')} ${hour}:${minute}:00+09:00`, 'YYYYMMDD HH:mm:ssZ');
+                // パフォーマンスを作成しない時刻に指定されていなかったら作成
+                if (noPerformanceTimes.indexOf(`${hour}${minute}`) < 0) {
+                    performanceInfos.push({
+                        door_time: startDate.toDate(),
+                        start_date: startDate.toDate(),
+                        end_date: moment(startDate).add(duration, 'minutes').toDate(),
+                        tour: tour,
+                        duration: moment.duration(duration, 'minutes').toISOString()
+                    });
+                }
+            });
         });
-    });
-    return info;
+    }
+    return performanceInfos;
 }
 /**
  *
--- app/controllers/performance.ts
+++ app/controllers/performance.ts
@@ -7,6 +7,8 @@ import * as ttts from '@motionpicture/ttts-domain';
 import * as createDebug from 'debug';
 import * as fs from 'fs-extra';
 import * as moment from 'moment';
+// tslint:disable-next-line:no-require-imports no-var-requires
+require('moment-timezone');
 
 const debug = createDebug('ttts-jobs:controller:performance');
 
@@ -14,7 +16,6 @@ const debug = createDebug('ttts-jobs:controller:performance');
  * 設定からパフォーマンスデータを作成する
  * @memberof controller/performance
  */
-// tslint:disable-next-line:max-func-body-length
 export async function createFromSetting(): Promise<void> {
     // 作成情報取得
     const setting: any = fs.readJsonSync(`${process.cwd()}/data/${process.env.NODE_ENV}/setting.json`);
@@ -22,14 +23,10 @@ export async function createFromSetting(): Promise<void> {
 
     // 引数情報取得
     const targetInfo = getTargetInfoForCreateFromSetting(setting.performance_duration, setting.no_performance_times);
-    const times = targetInfo.times;
-    const days = targetInfo.days;
     debug('targetInfo:', targetInfo);
 
     // 劇場とスクリーン情報取得
-    const screenOfPerformance = await ttts.Models.Screen.findById(setting.screen)
-        .populate('theater')
-        .exec();
+    const screenOfPerformance = await ttts.Models.Screen.findById(setting.screen).populate('theater').exec();
     debug('screenOfPerformance:', screenOfPerformance);
     if (screenOfPerformance === null) {
         throw new Error('screen not found.');
@@ -47,73 +44,80 @@ export async function createFromSetting(): Promise<void> {
     const savePerformances: ttts.factory.performance.IPerformance[] = [];
 
     // 7日分Loop
-    const promisesDay = (days.map(async (day: string) => {
-        // 開始時間分Loop
-        const promisesTime = (times.map(async (time: any) => {
-            // 2017/10 2次 予約枠、時間の変更対応
-            const screen =
-                (setting.special_screens[time.start_time] !== undefined) ?
-                    setting.special_screens[time.start_time] : setting.screen;
+    await Promise.all(targetInfo.map(async (performanceInfo) => {
+        const day = moment(performanceInfo.start_date).tz('Asia/Tokyo').format('YYYYMMDD');
+        const startTime = moment(performanceInfo.start_date).tz('Asia/Tokyo').format('HHmm');
+        const endTime = moment(performanceInfo.end_date).tz('Asia/Tokyo').format('HHmm');
+        // 2017/10 2次 予約枠、時間の変更対応
+        const screen = (setting.special_screens[startTime] !== undefined) ? setting.special_screens[startTime] : setting.screen;
 
-            // ユニークなIDを生成
-            const id = [
-                // tslint:disable-next-line:no-magic-numbers
-                day.slice(-6),
-                setting.film,
-                screen,
-                time.start_time
-            ].join('');
+        // ユニークなIDを生成
+        const id = [
+            // tslint:disable-next-line:no-magic-numbers
+            day.slice(-6),
+            setting.film,
+            screen,
+            startTime
+        ].join('');
 
-            // パフォーマンス登録
-            const performance: ttts.factory.performance.IPerformance = {
-                id: id,
-                theater: screenOfPerformance.get('theater').get('id'),
-                theater_name: screenOfPerformance.get('theater').get('name'),
-                screen: screen,
-                screen_name: screenOfPerformance.get('name'),
-                film: film.get('id'),
-                ticket_type_group: setting.ticket_type_group,
-                day: day,
-                open_time: time.open_time,
-                start_time: time.start_time,
-                end_time: time.end_time,
-                canceled: false,
-                ttts_extension: {
-                    tour_number: time.tour_number,
-                    ev_service_status: ttts.factory.performance.EvServiceStatus.Normal,
-                    ev_service_update_user: '',
-                    online_sales_status: ttts.factory.performance.OnlineSalesStatus.Normal,
-                    online_sales_update_user: '',
-                    refund_status: ttts.factory.performance.RefundStatus.None,
-                    refund_update_user: '',
-                    refunded_count: 0
-                }
-            };
+        // パフォーマンス登録
+        const performance: ttts.factory.performance.IPerformance = {
+            id: id,
+            theater: screenOfPerformance.get('theater').get('id'),
+            theater_name: screenOfPerformance.get('theater').get('name'),
+            screen: screen,
+            screen_name: screenOfPerformance.get('name'),
+            film: film.get('id'),
+            ticket_type_group: setting.ticket_type_group,
+            day: day,
+            open_time: startTime,
+            start_time: startTime,
+            end_time: endTime,
+            canceled: false,
+            ttts_extension: {
+                tour_number: performanceInfo.tour.code,
+                ev_service_status: ttts.factory.performance.EvServiceStatus.Normal,
+                ev_service_update_user: '',
+                online_sales_status: ttts.factory.performance.OnlineSalesStatus.Normal,
+                online_sales_update_user: '',
+                refund_status: ttts.factory.performance.RefundStatus.None,
+                refund_update_user: '',
+                refunded_count: 0
+            },
+            door_time: performanceInfo.door_time,
+            start_date: performanceInfo.start_date,
+            end_date: performanceInfo.end_date,
+            duration: performanceInfo.duration,
+            tour: performanceInfo.tour
+        };
 
-            debug('creating performance...', performance);
-            //スクリーン、作品、上映日、開始時間
-            await performanceRepo.saveIfNotExists(performance);
-            savePerformances.push(performance);
-        }));
-        await Promise.all(promisesTime);
+        debug('creating performance...', performance);
+        await performanceRepo.saveIfNotExists(performance);
+        savePerformances.push(performance);
     }));
-    await Promise.all(promisesDay);
 }
+
+export interface ITargetPerformanceInfo {
+    door_time: Date;
+    start_date: Date;
+    end_date: Date;
+    duration: string;
+    tour: ttts.factory.performance.ITour;
+}
+
 /**
  * パフォーマンス作成・作成対象情報取得
- *
  * @memberof controller/performance
  */
-function getTargetInfoForCreateFromSetting(duration: number, noPerformanceTimes: string[]): any {
-    const info: any = {};
-    info.days = [];
-    info.times = [];
+function getTargetInfoForCreateFromSetting(duration: number, noPerformanceTimes: string[]): ITargetPerformanceInfo[] {
+    const performanceInfos: ITargetPerformanceInfo[] = [];
 
     // 引数から作成対象時間と作成日数を取得
     const argvLength: number = 5;
     if (process.argv.length < argvLength) {
         throw new Error('argv \'time\' or \'days\' not found.');
     }
+
     const indexTargetHours: number = 2;
     const indexStartDay: number = 3;
     const indexTargetDays: number = 4;
@@ -124,42 +128,52 @@ function getTargetInfoForCreateFromSetting(duration: number, noPerformanceTimes:
     // 何日分作成するか: 7
     const days: number = Number(process.argv[indexTargetDays]);
 
+    const minutes = ['00', '15', '30', '45'];
+    const tours = ['1', '2', '3', '4'];
+
     // 本日日付+開始日までの日数から作成開始日セット
-    const today = moment().add(start - 1, 'days');
     // 作成日数分の作成対象日付作成
     for (let index = 0; index < days; index = index + 1) {
-        const dateWk: string = today.add(1, 'days').format('YYYYMMDD');
-        info.days.push(dateWk);
-    }
-    const minutes: string[] = ['00', '15', '30', '45'];
-    const tours: string[] = ['1', '2', '3', '4'];
-    const hourLength: number = 2;
-    // 終了時刻取得
-    const getEndTime = (startTime: string) => {
-        const startMoment: moment.Moment = moment(startTime, 'HHmm');
+        const now = moment().add(start + index, 'days');
 
-        return moment(startMoment.add(duration, 'minutes'), 'HHmm').format('HHmm');
-    };
-    hours.forEach((hour) => {
-        // 2桁でない時は'0'詰め
-        hour = (hour.length < hourLength) ? `0${hour}` : hour;
-        let index: number = 0;
-        minutes.forEach((minute) => {
-            const startTime: string = hour + minute;
-            // パフォーマンスを作成しない時刻に指定されていなかったら作成
-            if (noPerformanceTimes.indexOf(startTime) < 0) {
-                info.times.push({
-                    open_time: startTime,
-                    start_time: startTime,
-                    end_time: getEndTime(startTime),
-                    tour_number: `${hour}${tours[index]}`
-                });
-            }
-            index += 1;
+        hours.forEach((hour) => {
+            // 2桁でない時は'0'詰め
+            // tslint:disable-next-line:no-magic-numbers
+            hour = `0${hour}`.slice(-2);
+            const tourStartDate = moment(`${now.format('YYYYMMDD')} ${hour}:00:00+09:00`, 'YYYYMMDD HH:mm:ssZ');
+
+            minutes.forEach((minute, minuteIndex) => {
+                // ツアー情報作成
+                const tourCode = `${hour}${tours[minuteIndex]}`;
+                const tour: ttts.factory.performance.ITour = {
+                    name: {
+                        en: `Tour${tourCode}`,
+                        ja: `ツアー${tourCode}`
+                    },
+                    code: `${tourCode}`,
+                    door_time: tourStartDate.toDate(),
+                    start_date: tourStartDate.toDate(),
+                    end_date: moment(tourStartDate).add(1, 'hour').toDate(),
+                    duration: moment.duration(1, 'hour').toISOString()
+                };
+
+                const startDate = moment(`${now.format('YYYYMMDD')} ${hour}:${minute}:00+09:00`, 'YYYYMMDD HH:mm:ssZ');
+
+                // パフォーマンスを作成しない時刻に指定されていなかったら作成
+                if (noPerformanceTimes.indexOf(`${hour}${minute}`) < 0) {
+                    performanceInfos.push({
+                        door_time: startDate.toDate(),
+                        start_date: startDate.toDate(),
+                        end_date: moment(startDate).add(duration, 'minutes').toDate(),
+                        tour: tour,
+                        duration: moment.duration(duration, 'minutes').toISOString()
+                    });
+                }
+            });
         });
-    });
+    }
 
-    return info;
+    return performanceInfos;
 }
 
 /**
